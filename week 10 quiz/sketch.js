241529dec3e1dfd02c266d2d76950b20cdc4d352
chacecang <czha0270@uni.sydney.edu.au>
2023-10-27 19:06:01 +1100

    Update sketch.js

--- week 10 quiz/sketch.js
+++ week 10 quiz/sketch.js
@@ -12,15 +12,16 @@ function preload() {
 function setup() {
   createCanvas(500, 500);
   layer = createGraphics(width, height);
-
   background('black');
   fft = new p5.FFT(0, 256);
 
+  // Calculate the rotational step for each frame
   a = 360 / (song.duration() * fr);
   b = a;
 
   frameRate(fr);
 
+  // Create a button for toggling audio play and pause
   btn = createButton('Play');
   btn.position(20, 520);
   btn.mousePressed(toggleAudio);
@@ -28,22 +29,29 @@ function setup() {
 
 function draw() {
   background(0);
+  
+  // Analyze the song to get amplitude values and prepare them for visualization
   const spectrumB = fft.analyze().reverse();
   spectrumB.splice(0, 40);
 
   push();
   translate(250, 250);
 
+  // Draw the dynamic square, circle, and another square visuals based on the song amplitude
   drawDynamicSquare(spectrumB, 150);
   drawDynamicCircle(spectrumB, 100);
   drawDynamicSquare(spectrumB, 50);
 
   pop();
 
+  // Adjust rotation for the next frame if the song is playing
   if (playing) a += b;
+  
+  // Show the progress bar for the song
   displayProgressBar();
 }
 
+// Function to visualize the amplitude values as a dynamic square shape
 function drawDynamicSquare(spectrumB, size) {
     beginShape();
 
@@ -82,6 +90,7 @@ function drawDynamicSquare(spectrumB, size) {
     endShape(CLOSE);
 }
 
+// Function to visualize the amplitude values as a dynamic circle shape
 function drawDynamicCircle(spectrumB, radius) {
     beginShape();
     for(let i = 0; i < spectrumB.length; i++){
@@ -94,6 +103,7 @@ function drawDynamicCircle(spectrumB, radius) {
     endShape(CLOSE);
 }
 
+// Function to display the song's playback progress as a progress bar
 function displayProgressBar() {
   let progress = song.currentTime() / song.duration();
   let barHeight = 10;
@@ -105,6 +115,7 @@ function displayProgressBar() {
   rect(0, height - barHeight, width * progress, barHeight);
 }
 
+// Toggle between play and pause for the song
 function toggleAudio() {
   playing ? song.pause() : song.play();
   btn.html(playing ? 'Play' : 'Pause');
